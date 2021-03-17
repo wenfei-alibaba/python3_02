07a173788cdd81cd02fce7a4ba8b8cacbc5d7b9c
wenfei-alibaba <xuwenfei2020@aliyun.com>
2021-03-17 23:37:08 +0800

    Initial commit

---
+++ 02-运算符.py
@@ -0,0 +1,27 @@
+# 1.定义一个变量用来记录QQ号
+qq_number = "2565728258"
+qq_pwd = "Xl1996124@#$"
+print(qq_number)
+print(qq_pwd)
+print("-----------------")
+# 2.苹果定价
+price = 80
+weight = 7.7
+money = price*weight
+money = money -6
+print(money)
+print("-----------------")
+
+"""
+姓名：小明
+年龄：18 岁
+性别：是男生
+身高：1.75 米
+体重：75.0 公斤
+"""
+
+name = "小明"
+age = 19
+height = 1.77
+weight = 78
+print(name)
---
+++ 04-苹果案例优化.py
@@ -0,0 +1,6 @@
+# 1.提示用户输入苹果的单价
+price = float(input("苹果的单价："))
+# 2.提示用户输入苹果的重量
+weight = float(input("苹果的重量："))
+#3.计算金额
+print(weight*price)
